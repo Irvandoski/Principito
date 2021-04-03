95fe0138bba3e3512fd8cbccbe7c46550d104ac9
Irving Juárez <Deltairving55@hotmail.com>
2021-04-03 00:39:24 -0700

    Persona 1 - Irving

--- Principito/Main.py
+++ Principito/Main.py
@@ -12,18 +12,25 @@ class Main(object):
         self.width = self.screen.GetSystemMetrics(0)
         self.height = self.screen.GetSystemMetrics(1)
         self.G_principito_front = Objetos.Principito(0.30)
-        self.G_escenas = Escenarios.Escenas(); 
-
+        self.G_escenas = Escenarios.Escenas()
+        self.G_personas = Objetos.Personas(0.40)
+        self.nivel = 0
     def keyPress(self,bkey,x,y):
         key = bkey.decode("utf-8")
         #SALIR
         if key == chr(27):
             os._exit(1) 
             sys.exit()
-        if key == 'q':
+        if key == 'q' and self.nivel == 1:
+            glClearColor(0.027, 0.823, 0.835, 0.0)
+            glutDisplayFunc(self.G_personas.Silueta_1)
+            glFlush()
+            self.nivel +=1;
+        if key == 'q' and self.nivel == 0:
             glClearColor(0.027, 0.823, 0.835, 0.0)
             glutDisplayFunc(self.G_escenas.desierto)
             glFlush()
+            self.nivel +=1;
     def main(self):
         glutInit(sys.argv)
         glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
